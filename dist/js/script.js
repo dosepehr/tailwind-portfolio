4f7a222ed6d71b279f4fb842c623ae7611e00833
dosepehr <dosepehr@gmail.com>
2023-01-26 11:31:51 +0330

    done navlinks active effect

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,31 @@ ham.onclick = () => {
     ham.classList.toggle('open');
     ham_menu.classList.toggle('active')
 }
+
+
+
+
+
+
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navLink');
+let current
+// console.log(navLink);
+
+window.addEventListener('scroll', function () {
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight/3) {
+            current = section.getAttribute('id')
+        }
+    })
+    console.log(current)
+    navLinks.forEach(item => {
+        item.classList.remove("active")
+        if (item.classList.contains(current)) {
+            item.classList.add('active')
+        }
+    })
+})
