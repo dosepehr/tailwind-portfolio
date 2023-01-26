168064d33afce36cd353193815922a80c887e7a5
dosepehr <dosepehr@gmail.com>
2023-01-26 13:02:32 +0330

    solve some minor problems

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,27 @@
-const ham=document.querySelector('.ham')
+// ham menu
+const ham = document.querySelector('.ham')
 const ham_menu = document.querySelector('.ham-menu')
+const body=document.querySelector('body')
 
 
-
-ham.onclick = () => {
+ham.onclick = (e) => {
+    e.stopPropagation()
     ham.classList.toggle('open');
     ham_menu.classList.toggle('active')
+}
+body.onclick = () => {
+    ham.classList.remove('open')
+    ham_menu.classList.remove('active')
 }
 
 
 
 
-
-
+// navlinks active effect
 
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('.navLink');
 let current
-// console.log(navLink);
 
 window.addEventListener('scroll', function () {
     sections.forEach(section => {
@@ -27,7 +31,6 @@ window.addEventListener('scroll', function () {
             current = section.getAttribute('id')
         }
     })
-    console.log(current)
     navLinks.forEach(item => {
         item.classList.remove("active")
         if (item.classList.contains(current)) {
